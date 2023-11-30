29a5691c3f9d59627541e4f7e184779716a3969b
joaotomas007 <joaotomasgomes@gmail.com>
2023-11-30 00:05:42 +0000

    zz

--- project.cpp
+++ project.cpp
@@ -14,7 +14,8 @@ void readinput(){
     scanf("%d", &n);
     vector<vector<int>> vect(n, vector<int>(3));
     for(int i = 0; i < n; i++){
-     scanf("%d %d %d", &vect[i][0], &vect[i][1], &vect[i][2]);
+        scanf("%d %d %d", &vect[i][0], &vect[i][1], &vect[i][2]);
+        printf("%d %d %d\n",  vect[i][0], vect[i][1], vect[i][2]);
     }
 }
 
