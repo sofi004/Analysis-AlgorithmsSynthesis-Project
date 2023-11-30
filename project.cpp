f277e08b9101f487bfdd5627064adc2207d65da4
joaotomas007 <joaotomasgomes@gmail.com>
2023-11-30 00:28:00 +0000

    zz

--- project.cpp
+++ project.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <stdio.h>
+#include <cstring>
 
 using namespace std;
 
 int X, Y;
 vector<vector<int>> vect;
+vector<vector<int>> matrix;
 
 
 void readinput(){
@@ -13,12 +15,23 @@ void readinput(){
     scanf("%d %d", &X, &Y);
     scanf("%d", &n);
     vector<vector<int>> vect(n, vector<int>(3));
+    vector<vector<int>> matrix(X, vector<int>(Y, 0));
+
     for(int i = 0; i < n; i++){
         scanf("%d %d %d", &vect[i][0], &vect[i][1], &vect[i][2]);
+        matrix[vect[i][0]][vect[i][1]] = vect[i][2];
         printf("%d %d %d\n",  vect[i][0], vect[i][1], vect[i][2]);
     }
 }
 
 int main(){
     readinput();
+    for (int i = 0; i < X; i++) {
+        for (int j = 0; j < Y; j++) {
+            printf("%d", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
