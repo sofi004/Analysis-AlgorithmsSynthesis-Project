996f15a2e35b5e28258db28a75d262e93640e7a6
ist1106194 <sophiapiteira@gmail.com>
2023-12-19 12:12:17 +0000

    ee

--- Projeto2/notV2.cpp
+++ Projeto2/notV2.cpp
@@ -5,21 +5,24 @@
 
 using namespace std;
 
-int N, M;
+int N, M, sccCount;
 vector<vector<int>> matrix;
+vector<vector<int>> transpose;
+vector<vector<int>> dag;
+vector<int> sccVector;
 
 void readinput() {
     int trash;
     trash = scanf("%d %d", &N, &M);
     (void)trash;
     matrix = vector<vector<int>>(N, vector<int>());
+    transpose = vector<vector<int>>(N, vector<int>());
     int x_temp, y_temp;
     for (int i = 0; i < M; i++) {
         trash = scanf("%d %d", &x_temp, &y_temp);
         (void)trash;
-        if (x_temp > 0 && y_temp > 0) {
-            matrix[x_temp - 1].push_back(y_temp - 1);
-        }
+        matrix[x_temp - 1].push_back(y_temp - 1);
+        transpose[y_temp - 1].push_back(x_temp - 1);
     }
 }
 
@@ -29,8 +32,38 @@ void readinput() {
 //topologimente ordenados. 
 //Para isso só precisamos de ter conhecimento da stack endValues e da matrix.
 void findSCCs(stack<int> endValues){
-    int sccCount = 0;
-    vector<int> onStackS = vector<int>(N, -1);
+    sccCount = 0;
+    stack<int> aux;
+    vector<bool> onAux = vector<bool>(N, false);
+    sccVector = vector<int>(N, -1);
+    vector<bool> visited = vector<bool>(N, false);
+    while(!endValues.empty()){
+        int i = endValues.top();
+        endValues.pop();
+        if (!visited[i] && !onAux[i]) {
+            aux.push(i);
+            while (!aux.empty()) {
+                int temp = aux.top();
+                if(!visited[temp] && !onAux[temp]){
+                    onAux[temp] = true;
+                    for (int next : transpose[temp]) {
+                        if (!visited[next] && !onAux[next]) {
+                            aux.push(next);
+                        }
+                    
+                    }
+                } else{
+                    if(onAux[temp]){
+                        visited[temp] = true;
+                        sccVector[temp] = sccCount;
+                    }
+                    aux.pop();
+
+                }
+            }
+            sccCount++;
+        }
+    }
 }
 
 
@@ -39,7 +72,6 @@ void dfs() {
     stack<int> endValues;
     vector<bool> onStackS = vector<bool>(N, false);
     vector<bool> visited = vector<bool>(N, false);
-
     for (int i = 0; i < N; i++) {
         if (!visited[i]) {
             s.push(i);
@@ -48,10 +80,8 @@ void dfs() {
             while (!s.empty()) {
                 int temp = s.top();
                 bool hasUnvisitedNeighbor = false;
-
                 for (int j = 0; j < matrix[temp].size(); j++) {
-                    int next = matrix[temp][j];
-
+                    int next = matrix[temp][j]; 
                     if (!visited[next]) {
                         if (!onStackS[next]) {
                             s.push(next);
@@ -74,50 +104,41 @@ void dfs() {
         }
     }
     findSCCs(endValues);
-
-    while (!endValues.empty()) {
-    int node = endValues.top();
-    endValues.pop();
-    cout << "Node: " << node << endl;
-    }
 }
 
-/*
+
 void builtMatrix(){
-    dag = vector<vector<int>>(sccCount, vector<int>(sccCount, 0));
+    dag = vector<vector<int>>(sccCount, vector<int>());
     for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            if(matrix[i][j] == 1){
-                if(low[i] != low[j]){
-                    dag[low[i]][low[j]] = 1;
-                }
+        for (int j : matrix[i]) {
+        if(sccVector[i]!=sccVector[j]){
+                dag[sccVector[i]].push_back(sccVector[j]);
             }
         }
     }
 }
+
 
 void longestPath(){
     int maximum = 0;
     vector<int> longestPathList = vector<int>(sccCount, 0);
     for(int i = sccCount -1; i >= 0; i--){
-        for(int j = 0; j < sccCount; j++){
-            if(dag[i][j] == 1){
-                longestPathList[i] = max(longestPathList[i], longestPathList[j] +1);
-                if(longestPathList[i] > maximum){
-                    maximum = longestPathList[i];
-                }
+        for(int j : dag[i]){
+            longestPathList[i] = max(longestPathList[i], longestPathList[j] +1);
+            if(longestPathList[i] > maximum){
+                maximum = longestPathList[i];
             }
         }
     }
     printf("%d\n", maximum);
 }
-*/
+
 
 
 int main() {
     readinput();
     dfs();
-    //builtMatrix();
-    //longestPath();
+    builtMatrix();
+    longestPath();
     return 0;
 }
