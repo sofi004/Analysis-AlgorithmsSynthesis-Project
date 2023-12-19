3952e27cf70061323499faec5d4c39b5c09fb7c5
ist1106194 <sophiapiteira@gmail.com>
2023-12-19 14:42:39 +0000

    ee

--- Projeto2/project.cpp
+++ Projeto2/project.cpp
@@ -1,93 +1,129 @@
 #include <iostream>
 #include <vector>
-#include <stdio.h>
-#include <cstring>
 #include <stack>
-#include <algorithm>
 
 using namespace std;
-int N, M;
-vector<vector<int>> matrix;
-// -1 = unvisited
-int id = 0;
-int sccCount = 0;
-stack<int> tarjanStack;
-vector<int> ids;
-vector<int> low;
-vector<bool> onStack;
 
-void readinput(){
+int N, M, sccCount;
+vector<vector<int>> adjList, transpose, dag;
+vector<int> sccVector;
+
+void readinput() {
     int trash;
     trash = scanf("%d %d", &N, &M);
-    printf("%d %d\n", N, M);
-    (void) trash;
-    matrix = vector<vector<int>> (N, vector<int>(N, 0));
+    (void)trash;
+    adjList = vector<vector<int>>(N, vector<int>());
+    transpose = vector<vector<int>>(N, vector<int>());
     int x_temp, y_temp;
-    for(int i = 0; i < M; i++){
+    for (int i = 0; i < M; i++) {
         trash = scanf("%d %d", &x_temp, &y_temp);
-        (void) trash;
-        printf("%d %d\n", x_temp, y_temp);
-        if(((x_temp -1) < N) && ((y_temp -1) < N) && ((x_temp) > 0) && ((y_temp) > 0)){
-            matrix[x_temp -1][y_temp -1] = 1;
-            printf("%d %d %d\n", x_temp, y_temp, matrix[x_temp -1][y_temp -1]);
-        }
-    }
-    ids = vector<int>(N, -1);
-    low = vector<int>(N, 0);
-    onStack = vector<bool>(N, false);
-}
-
-void dfs(int currentId){
-    tarjanStack.push(currentId);
-    onStack[currentId] = true;
-    ids[currentId] = low[currentId] = id++;
-    for(int neighbor = 0; neighbor < N; neighbor++){
-        if(matrix[currentId][neighbor] == 1){
-            if(ids[neighbor] == -1){
-                dfs(neighbor);
-            }
-            if(onStack[neighbor]){
-                low[currentId] = min(low[currentId], low[neighbor]);
-            }
-        }
-    }
-
-    if(ids[currentId] == low[currentId]){
-        for (; !tarjanStack.empty(); ) {
-            int node = tarjanStack.top();
-            tarjanStack.pop();
-            onStack[node] = false;
-            low[node] = ids[currentId];
-            if (node == currentId) break;
-        }
-        sccCount++;
+        (void)trash;
+        adjList[x_temp - 1].push_back(y_temp - 1);
+        transpose[y_temp - 1].push_back(x_temp - 1);
     }
 }
 
-vector <int> findSCC(){
+void findSCCs(stack<int> endValues){
+    sccCount = 0;
+    sccVector = vector<int>(N, -1);
+    stack<int> aux;
+    vector<bool> onAux = vector<bool>(N, false);
+    vector<bool> visited = vector<bool>(N, false);
+    while(!endValues.empty()){
+        int i = endValues.top();
+        endValues.pop();
+        if (!visited[i] && !onAux[i]) {
+            aux.push(i);
+            while (!aux.empty()) {
+                int temp = aux.top();
+                if(!visited[temp] && !onAux[temp]){
+                    onAux[temp] = true;
+                    for (int next : transpose[temp]) {
+                        if (!visited[next] && !onAux[next]) {
+                            aux.push(next);
+                        }
+                    
+                    }
+                } else{
+                    if(onAux[temp]){
+                        visited[temp] = true;
+                        sccVector[temp] = sccCount;
+                    }
+                    aux.pop();
+
+                }
+            }
+            sccCount++;
+        }
+    }
+}
+
+void dfs() {
+    stack<int> s;
+    stack<int> endValues;
+    vector<bool> onStackS = vector<bool>(N, false);
+    vector<bool> visited = vector<bool>(N, false);
+    for (int i = 0; i < N; i++) {
+        if (!visited[i]) {
+            s.push(i);
+            onStackS[i] = true;
+            while (!s.empty()) {
+                int temp = s.top();
+                bool hasUnvisitedNeighbor = false;
+                for (int next : adjList[temp]) {
+                    if (!visited[next]) {
+                        if (!onStackS[next]) {
+                            s.push(next);
+                            onStackS[next] = true;
+                            hasUnvisitedNeighbor = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!hasUnvisitedNeighbor) {
+                    s.pop();
+                    onStackS[temp] = false;
+                    if (!visited[temp]) {
+                        visited[temp] = true;
+                        endValues.push(temp);
+                    }
+                }
+            }
+        }
+    }
+    findSCCs(endValues);
+}
+
+void builtDag(){
+    dag = vector<vector<int>>(sccCount, vector<int>());
     for(int i = 0; i < N; i++){
-        if(ids[i] == -1){
-            dfs(i);
+        for (int j : adjList[i]) {
+        if(sccVector[i]!=sccVector[j]){
+                dag[sccVector[i]].push_back(sccVector[j]);
+            }
         }
     }
-    return low;
 }
 
-int main(){
-    printf("ola\n");
+void longestPath(){
+    int maximum = 0;
+    vector<int> longestPathList = vector<int>(sccCount, 0);
+    for(int i = sccCount -1; i >= 0; i--){
+        for(int j : dag[i]){
+            longestPathList[i] = max(longestPathList[i], longestPathList[j] +1);
+            if(longestPathList[i] > maximum){
+                maximum = longestPathList[i];
+            }
+        }
+    }
+    printf("%d\n", maximum);
+}
+
+int main() {
     readinput();
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-    findSCC();
-    printf("\n");
-    printf("\n");
-    for(int i = 0; i < N; i++){
-        printf("%d ", low[i]);
-    }
-    printf("\n");
+    dfs();
+    builtDag();
+    longestPath();
     return 0;
 }
