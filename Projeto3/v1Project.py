d856b6fb937152417a30cc9fa7147a5942f160b3
João Gomes <joaotomasgomes@gmail.com>
2024-01-06 18:53:14 +0000

    zz

--- Projeto3/v1Project.py
+++ Projeto3/v1Project.py
@@ -7,25 +7,25 @@ dp = {}
 
 for b in range(nb):
     l , c = map(int, input().split())
-    x = LpVariable(b, 0, upBound = c, cat=LpInteger)
+    x = LpVariable('B' + str(b), 0, upBound = c, cat=LpInteger)
     db.update({b : [l, c, x]})
     
 for p in range(np):
     i, j, k, lp = map(int, input().split())
-    x = LpVariable(p, 0, upBound = min(db[i][1], db[j][1], db[k][1]), cat = LpInteger) 
-    if (db[i][0] + db[j][0] + db[k][0]) < lp:
-        dp.update({p : [i, j, k, lp, x]})
-        db[i] += [x]
-        db[i] += [x]
-        db[i] += [x]
+    y = LpVariable('P' + str(p) , 0, upBound = min(db[i - 1][1], db[j - 1][1], db[k - 1][1]), cat = LpInteger) 
+    if (db[i  - 1][0] + db[j -1 ][0] + db[k - 1][0]) < lp:
+        dp.update({p : [i, j, k, lp, y]})
+        db[i - 1].append(y)
+        db[j - 1].append(y)
+        db[k - 1].append(y)
 
-prob += lpSum([cb[0]*cb[2] for cb in db] + [cp[3]*cp[4]for cp in dp]) 
+prob += lpSum([cb[0]*cb[2] for cb in db.values()] + [cp[3]*cp[4]for cp in dp.values()]) 
 
-prob += lpsum([cb[2] for cb in db] + [3*cp[4]for cp in dp]) <= limProd
+prob += lpSum([cb[2] for cb in db.values()] + [3*cp[4]for cp in dp.values()])<= limProd
 
-for cb in db:
-    prob += lpsum(cb[2:]) <= cb[1]
+for cb in db.values():
+    prob += lpSum(cb[2:]) <= cb[1]
 
-model.solve(GLPK(msg=0))
+prob.solve(GLPK(msg=0))
 
 print(int(value(prob.objective)))
